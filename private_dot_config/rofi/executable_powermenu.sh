224bc34869a36f01bd4da3021bc832c270a48ab8
AllenWu233 <allenwu233@outlook.com>
2025-03-12 23:00:00 +0800

    Backup 2025-03-12 23:00:00

--- private_dot_config/rofi/executable_powermenu.sh
+++ private_dot_config/rofi/executable_powermenu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rofi -show p -modi p:~/.config/rofi/off.sh -theme ~/.config/rofi/nord.rasi -dpi 1
-# rofi -show p -modi p:~/.config/rofi/off.sh -dpi 1
+# rofi -show p -modi p:~/.config/rofi/off.sh -theme ~/.config/rofi/nord.rasi -dpi 1
+rofi -show p -modi p:~/.config/rofi/off.sh -dpi 1
