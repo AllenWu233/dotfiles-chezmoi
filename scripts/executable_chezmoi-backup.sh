222fcbd71326236472da643814452967a47493dc
AllenWu233 <allenwu233@outlook.com>
2025-03-14 22:44:44 +0800

    Add .aria2/dht.dat Add .aria2/dht6.dat Remove .config/alacritty/themes/catppuccin-mocha.toml Remove .config/alacritty/themes/tokyo-night-storm.toml Remove .config/alacritty/themes/tokyo-night.old.toml Remove .config/alacritty/themes/tokyo-night.toml Add .config/dunst/dunstrc-gruvbox-dark Remove .config/fastfetch/config.jsonc Remove .config/fastfetch/config.jsonc.2.bak Remove .config/fontconfig/fonts.conf.2.bak Remove .config/fontconfig/fonts.conf.bak Add .config/i3/appearence.conf Remove .config/i3/config.bak Add .config/i3/keybindings.conf Add .config/i3/startup.conf Add .config/i3/window_rules.conf Remove .config/picom/picom-ftlabs-pr41.conf Remove .config/picom/picom-ftlabs.conf Remove .config/yazi/flavors/catppuccin-latte.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/flavors/catppuccin-mocha.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/plugins/mdcat.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/plugins/mdcat.yazi/init.lua Remove .config/yazi/plugins/ouch.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/plugins/ouch.yazi/init.lua Remove .config/yazi/plugins/system-clipboard.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/plugins/system-clipboard.yazi/init.lua Remove .config/yazi/plugins/yamb.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Remove .config/yazi/plugins/yamb.yazi/init.lua Remove .config/zellij/config.kdl.bak Add .librewolf/native-messaging-hosts/org.keepassxc.keepassxc_browser.json Add .librewolf/wqbtt3vg.default-release/chrome/userChrome.css Update .zshrc Remove scripts/chezmoi-backup.sh Remove scripts/fix-network.sh

--- scripts/executable_chezmoi-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-chezmoi re-add
-cd "$HOME/.local/share/chezmoi" || exit
-git add .
-git commit -m "Backup $(date +'%Y-%m-%d %H:%M:%S')"
-git push origin main
--- scripts/executable_fix-network.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Fix network connect after resuming from hibernate
-MOD="r8169"
-rmmod "$MOD"
-modprobe "$MOD"
