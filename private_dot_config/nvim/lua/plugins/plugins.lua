13134ac18f4d91916af3f3aab77acc066a781f90
AllenWu233 <allenwu233@outlook.com>
2025-03-15 19:00:00 +0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/plugins.lua

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -1,17 +1,2 @@
 -- ----- Add Plugins ----- --
-return {
-  -- {
-  --   "ledger/vim-ledger",
-  --   ft = { "ledger" },
-  -- },
-
-  -- {
-  --   "nathangrigg/vim-beancount",
-  --   ft = { "beancount" },
-  -- },
-
-  -- {
-  --   "crispgm/cmp-beancount",
-  --   ft = { "beancount" },
-  -- },
-}
+return {}
